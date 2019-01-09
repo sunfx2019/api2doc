9f3aa7a4c0c61e03f122b2cfb7be7b8bc0703b44
sunfx <123074528@qq.com>
2019-01-09 16:26:11 +0800

    更细
    Signed-off-by: sunfx <123074528@qq.com>

--- src/main/java/com/api2doc/controller/UserController.java
+++ src/main/java/com/api2doc/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.api2doc.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller  
-public class UserController {  
-      
-    @RequestMapping("/index")      
-    public ModelAndView index(){        
-        ModelAndView view = new ModelAndView("index");     
-        view.addObject("id", System.currentTimeMillis());     
-        return view;      
-    }   
-}  
+import com.api2doc.bean.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping
+    @PostMapping
+    @ResponseBody
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public User addUser(String group, String name, String type) {
+    	User rser = new User();
+    	rser.setId("1");
+    	rser.setName("admin");
+        return rser;
+    }
+}
